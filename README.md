5cd4b8458623170bafb6ef358be4141d26a4d0f6
Leonardo Santos <leonardo@neemu.com>
2018-03-29 18:27:28 -0400

    feat: first version that works on local and deployed

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ Basic example code for running Python API on AWS Lambda, deploying with Serverle
 sam local start-api
 ```
 Request: http://localhost:3000/api/hello/world?looks=good
+
+# Deploying with Serverless
+
+```
+serverless deploy
+```
+
+Check the return of this command, it prints out an URL like this:
+https://nsm7example.execute-api.us-east-1.amazonaws.com/dev/api/{parameter1}/{parameter2}
--- handler.py
+++ handler.py
@@ -1,15 +1,27 @@
 import json
 
 def main(event, context):
-    pathParameters = event.get('pathParameters', {})
-    query = event.get('queryStringParameters', {})
+    # TODO: check a better way to do this check if is local
+    isLocal = 'pathParameters' in event
+
+    # when running local you must use `pathParameters`,
+    # but you should use `path` when deployed.
+    pathParameters = event.get('pathParameters', event.get('path', {}))
+
+    # when running local you must use `queryStringParameters`,
+    # but you should use `query` when deployed.
+    query = event.get('queryStringParameters', event.get('query', {}))
     print(json.dumps(event))
     response = {
         'pathParameters': pathParameters,
         'query': query
     }
-    return {
-        'statusCode': 200,
-        'headers': {},
-        'body': json.dumps(response)
-    }
+
+    if isLocal:
+        return {
+            'statusCode': 200,
+            'headers': {},
+            'body': json.dumps(response)
+        }
+    else:
+        return response
