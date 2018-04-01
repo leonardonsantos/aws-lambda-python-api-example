1260b2536664884ce6397e8e00f6993cab9a4fdd
Leonardo Santos <leonardo@neemu.com>
2018-04-01 11:29:47 -0400

    refact: use a handler for local and another for production
    The local handler just formats the diffenrent input and output for
    AWS Sam Local, but still calls main handler, that is used for production.

--- handler.py
+++ handler.py
@@ -1,27 +1,22 @@
 import json
 
-def main(event, context):
-    # TODO: check a better way to do this check if is local
-    isLocal = 'pathParameters' in event
-
-    # when running local you must use `pathParameters`,
-    # but you should use `path` when deployed.
-    pathParameters = event.get('pathParameters', event.get('path', {}))
-
-    # when running local you must use `queryStringParameters`,
-    # but you should use `query` when deployed.
-    query = event.get('queryStringParameters', event.get('query', {}))
-    print(json.dumps(event))
-    response = {
-        'pathParameters': pathParameters,
-        'query': query
+def local(event, context):
+    # Using AWS Sam Local, the input event and the return output are different.
+    # This function modifies event and pass it to main handler
+    event['path'] = event.get('pathParameters', {})
+    event['query'] = event.get('queryStringParameters', {})
+    body = main(event, context)
+    # must include one of: body, headers or statusCode in the response object
+    return {
+        'statusCode': 200,
+        'headers': {},
+        'body': json.dumps(body)
     }
 
-    if isLocal:
-        return {
-            'statusCode': 200,
-            'headers': {},
-            'body': json.dumps(response)
-        }
-    else:
-        return response
+def main(event, context):
+    path = event.get('path', {})
+    query = event.get('query', {})
+    return {
+        'path': path,
+        'query': query
+    }
